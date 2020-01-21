2911b6c3caf757eb55a8e4d7b44320ac387d47b5
CPPtrueCoder <andierodin@gmail.com>
2020-01-22 01:08:36 +0300

    Add functional for editing HTML, and synchronize

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -7,7 +7,7 @@ export default class Editor extends Component {
 	
 	constructor() {
 		super ();
-		this.currentPage="index.html";
+		this.currentPage = "index.html";
 		this.state = {
 			pageList: [],
 			newPageName: ""
@@ -16,22 +16,50 @@ export default class Editor extends Component {
 	}
 	
 	componentDidMount() {
-	this.init(this.currentPage);
+		this.init (this.currentPage);
 	}
 	
-	init(page){
-		this.iframe=document.querySelector('iframe');
-		this.open(page);
-		this.loadPageList();
-	}
-	open(page){
-		this.currentPage=`../${page}`;
-		this.iframe.load(this.currentPage,()=>{
-			console.log(this.currentPage);
-		});
+	init(page) {
+		this.iframe = document.querySelector ('iframe');
+		this.open (page);
+		this.loadPageList ();
 	}
 	
+	parseStrToDOM(str) {
+		const parser = new DOMParser ();
+		return parser.parseFromString (str, "text/html");
+	}
 	
+	open(page) {
+		this.currentPage = `../${page}`;
+		axios.get (`../${page}`).then (res =>  this.parseStrToDOM (res.data))
+		.then(this.wrapTextNodes)
+			.then(dom=>{
+				this.virtualDom=dom;
+				return dom;
+			})
+			.then(this.serializeDOMtoStr)
+			.then(html=>axios.post("./api/saveTempPage.php",{html}))
+			.then(()=>this.iframe.load("../temp.html"))
+			.then(()=>this.enableEditing())
+		// this.iframe.load (this.currentPage, () => {
+		//
+		// });
+	}
+	enableEditing(){
+		this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element=>{
+			element.contentEditable = "true";
+			element.addEventListener("input",()=>{
+				this.onTextEdit(element);
+			})
+		})
+	}
+	
+	onTextEdit(element){
+		const id =element.getAttribute("nodeid");
+		this.virtualDom.body.querySelector(`[nodeid=""${id}]`).innerHTML=element.innerHTML;
+		
+	}
 	loadPageList() {
 		axios
 			.get ("./api").then (res => this.setState ({pageList: res.data}))
@@ -39,16 +67,48 @@ export default class Editor extends Component {
 	
 	createNewPage() {
 		axios.post ("./api/createNewPage.php", {"name": this.state.newPageName})
-			.then (this.loadPageList() )
+			.then (this.loadPageList ())
 			.catch (() => alert ("Страница уже существует"));
 	}
 	
-	deletePage(page){
-		axios.post("./api/deletePage.php",{"name":page})
-			.then (this.loadPageList() )
+	deletePage(page) {
+		axios.post ("./api/deletePage.php", {"name": page})
+			.then (this.loadPageList ())
 			.catch (() => alert ("page not found"));
 	}
 	
+	wrapTextNodes(dom) {
+		const body = dom.body;
+		let textNodes = [];
+		
+		function recursy(element) {
+			element.childNodes.forEach (node => {
+				
+				if (node.nodeName === "#text" && node.nodeValue.replace (/\s+/g, "").length > 0) {
+					
+					textNodes.push (node);
+				}
+				else {
+					recursy (node);
+				}
+			})
+		}
+		
+		recursy (body);
+		textNodes.forEach ((node,i) => {
+			const wrapper = dom.createElement ('text-editor');
+			node.parentNode.replaceChild (wrapper, node);
+			wrapper.appendChild (node);
+			wrapper.setAttribute("nodeid",i);
+			
+		});
+		return dom;
+	}
+	serializeDOMtoStr(dom){
+		const serializer = new XMLSerializer();
+		
+		return  serializer.serializeToString(dom);
+	}
 	render() {
 		// const {pageList} = this.state;
 		// const pages = pageList.map ((page, i) => {
